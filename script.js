0775fd2a69a974d383a8ae935fca76f98a662db1
Oleksandr Lusenko <lusenko.sasha.2015@gmail.com>
2021-08-04 13:02:37 +0300

    made block with information

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-'use strict';
-
 let buttonModal = document.querySelector('.button-modal');
 let modal = document.querySelector('.modal');
+let firsInput = document.querySelector('.datepicker');
 let span = document.querySelector('.close-modal');
 let selectBlock = document.querySelector('.select-block');
+let select = document.querySelector('.select');
+let infoDay = document.querySelector('.info-day');
+let infoCar = document.querySelector('.info-car');
+let blockWithInfo = document.querySelector('.show-info');
 
 buttonModal.addEventListener('click', () => {
    modal.style.display = 'block';
 });
-span.addEventListener('click', () => {
+span.onclick = function (){
    modal.style.display = 'none';
-});
+}
 window.onclick = function(event) {
    if (event.target == modal) {
       modal.style.display = "none";
@@ -19,5 +22,21 @@ window.onclick = function(event) {
 const picker = datepicker('.datepicker', {
    onSelect: (instance, date) => {
       selectBlock.style.display = 'block';
+   }
+})
+let showDay;
+let showCar;
+function showInfo(){
+    showDay = firsInput.value;
+    infoDay.value = showDay;
+    showCar = select.value;
+    infoCar.value = showCar;
+}
+select.addEventListener('change', () => {
+   if(select.value == 'Audi' || select.value == 'BMW'
+   || select.value == 'Ford' || select.value == 'Kia'){
+      modal.style.display = 'none';
+      blockWithInfo.style.display = 'block';
+      showInfo();
    }
 })
